937d247165839619d30feed9648a8e930be3196c
minto5z <mintocse5@gmail.com>
2022-08-26 22:17:11 +0600

    logest substring

--- src/LongestSubstring.java
+++ src/LongestSubstring.java
@@ -1,5 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubstring {
     public static void main(String[] args) {
-        System.out.println("Longest Substring");
+        String s = "abcabcd";
+        Set<Character> set = new HashSet<>();
+        int left = 0, right = 0, max = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (set.add(c)) {
+                max = Math.max(max, (right - left + 1));
+                right++;
+            } else {
+                while (s.charAt(left) != c) {
+                    set.remove(s.charAt(left));
+                    left++;
+                }
+                set.remove(c);
+                left++;
+            }
+        }
+        System.out.println(max);
     }
 }
