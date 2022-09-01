baa37373a9f4e805e3ebf6f9ff29dd1154f95890
minto5z <mintocse5@gmail.com>
2022-09-02 00:07:18 +0600

    search insert position

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,24 @@
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        System.out.println(insertPosition(nums, target));
+    }
+
+    private static int insertPosition(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+
+}
